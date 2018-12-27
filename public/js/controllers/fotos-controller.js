f18b7da0391125a5510b5cfea788ba49fa8354d7
cesar.brasil <cesar.brasil@sabium.com>
2018-12-27 15:52:29 -0200

    aula 01

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,6 @@
+angular.module('alurapic').controller('FotosController', function($scope) {
+    $scope.foto = {
+        titulo: "Titulo da foto",
+        url: 'https://www.aprenderexcel.com.br//imagens/post/385/2901-1.jpg'
+    }
+})

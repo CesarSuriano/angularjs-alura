59ce1ece958aa4a9de12ddc512210044cd1454aa
César Suriano <cesaraugustokg6@hotmail.com>
2019-01-02 18:36:59 -0200

    foi

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,35 +1,28 @@
-angular.module('alurapic').controller('FotoController', function ($scope, $http, $routeParams) {
-    $scope.foto = {}
-    $scope.mensagem = ''
-
+angular.module('alurapic').controller('FotoController', function ($scope, $http, $routeParams, recursoFoto, cadastroDeFotos) {
+    $scope.foto = {};
+    $scope.mensagem = '';
 
     if ($routeParams.fotoId) {
-        $http.get('/v1/fotos/' + $routeParams.fotoId).success(function (foto) {
-            $scope.foto = foto
-        })
+        recursoFoto.get({ fotoId: $routeParams.fotoId }, function (foto) {
+            $scope.foto = foto;
+        }, function (erro) {
+            $scope.mensagem = 'Não foi possível obter a foto'
+        });
     }
 
     $scope.submeter = function () {
-        if ($scope.formulario.$valid) {
 
-            if ($scope.formulario.$valid) {
-                if ($routeParams.fotoId) {
-                    $http.put('/v1/fotos/' + $scope.foto._id, $scope.foto)
-                        .success(function () {
-                            $scope.mensagem = "Atualizou"
-                            $scope.mensagem = "Foto alterada"
-                            $scope.formulario.$setPristine();
-                        })
-                } else {
-                    $http.post('/v1/fotos', $scope.foto)
-                        .success(function (retorno) {
-                            $scope.mensagem = "Cadastrou brou"
-                        }).error(function (error) {
-                            $scope.mensagem = "Não foi possível cadastrar a foto"
-                            $scope.formulario.$setPristine();
-                        })
-                }
-            }
+        if ($scope.formulario.$valid) {
+            cadastroDeFotos.cadastrar($scope.foto)
+                .then(function (dados) {
+                    $scope.mensagem = dados.mensagem;
+                    if (dados.inclusao) $scope.foto = {};
+                })
+                .catch(function (erro) {
+                    $scope.mensagem = erro.mensagem;
+                }).finally(function(){
+                    $scope.formulario.$setPristine();
+                });
         }
-    }
+    };
 })
--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,23 +1,22 @@
-angular.module('alurapic').controller('FotosController', function ($scope, $http) {
+angular.module('alurapic').controller('FotosController', function ($scope, $http, recursoFoto) {
 
     $scope.mensagem = ""
 
     $scope.filtro = ''
-    $http.get('v1/fotos').success(function (retorno) {
+    recursoFoto.query(function (retorno) {
         $scope.fotos = retorno
-    }).error(function (error) {
+    }, function (error) {
         alert("Deu erro")
     })
 
     $scope.remover = function (foto) {
-        $http.delete('v1/fotos/' + foto._id)
-            .success(function () {
-                var indiceFoto = $scope.fotos.indexOf(foto)
-                $scope.fotos.splice(indiceFoto, 1)
-                $scope.mensagem = "Foto apagada"
-            }).error(function () {
-                alert("Deu erro")
-            })
+        recursoFoto.delete({ fotoId: foto._id }, function () {
+            var indiceFoto = $scope.fotos.indexOf(foto)
+            $scope.fotos.splice(indiceFoto, 1)
+            $scope.mensagem = "Foto apagada"
+        }, function () {
+            alert("Deu erro")
+        })
     }
 
 })
